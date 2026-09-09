4b1bc1f843b6bf4e0e20d2355ba470eddafcf0f7
TheMagmaCube <mailroxu@gmail.com>
2026-09-09 11:44:41 +0200

    fetch: PIC basic implementation (WIP)

--- source/kernel/lib/include/interrupt/pic.h
+++ source/kernel/lib/include/interrupt/pic.h
@@ -7,5 +7,16 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+typedef struct{
+
+    uint16_t PIC_Master_Command;
+    uint16_t PIC_Master_Data;
+
+    uint16_t PIC_Slave_Command;
+    uint16_t PIC_Slave_Data;
+
+} pic;
+
+void pic_init(pic *pic_instance);
 
 #endif
--- source/kernel/lib/src/interrupt/pic.c
+++ source/kernel/lib/src/interrupt/pic.c
@@ -5,6 +5,22 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-void pic_init(){
+typedef struct{
+
+    uint16_t PIC_Master_Command;
+    uint16_t PIC_Master_Data;
+
+    uint16_t PIC_Slave_Command;
+    uint16_t PIC_Slave_Data;
+
+} pic;
+
+void pic_init(pic *pic_instance){
+
+    pic_instance->PIC_Master_Command = 0x20;
+    pic_instance->PIC_Master_Data = 0x21;
+
+    pic_instance->PIC_Slave_Command = 0xA0;
+    pic_instance->PIC_Slave_Data = 0xA1;
 
 }
